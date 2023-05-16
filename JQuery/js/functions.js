a4316cbbde18a77c7b151604f98a81cab74d6804
Sandro Silva <81268287+santripas@users.noreply.github.com>
2023-05-15 21:13:33 -0300

    testando a função animate

--- JQuery/js/functions.js
+++ JQuery/js/functions.js
@@ -1,9 +1,16 @@
 $(function(){
+    
+    /*$('.box').fadeOut(2000,function(){
+        $('.box2').fadeIn(3000,function(){
+            console.log('Terminamos a nossa animação!');
+        });
+    
+    });
+    */
+
+    $('.box1').click(function(){
+        $('.box2').slideToggle();
+    })
+
    
-   //var teste = $('<h3>Meu conteúdo</h3>').appendTo($('.box')).css('color','red');
-
-  // $('.box').prepend('<h3>Olá mundo!</h3>');
-
-    $('.box').eq(1).remove();
-
 });
