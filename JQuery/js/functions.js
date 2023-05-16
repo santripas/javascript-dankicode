db06450feb36f8cc907874f94581c5842a1b9315
Sandro Silva <81268287+santripas@users.noreply.github.com>
2023-05-16 19:29:33 -0300

    testando a função de clonar e mover elementos com JQuery

--- JQuery/js/functions.js
+++ JQuery/js/functions.js
@@ -1,26 +1,6 @@
 $(function(){
     
-    /*$('.box').fadeOut(2000,function(){
-        $('.box2').fadeIn(3000,function(){
-            console.log('Terminamos a nossa animação!');
-        });
-    
-    });
-    */
+    var el = $('#source .texto').clone();
 
-    var timeOut = function(){
-        $('.box2').animate({
-            'width':'40%',
-            'height':'500px'
-        },2000);
-    }
-
-    $('.box1').animate({
-        'width':'40%',
-        'height':'500px'
-    },2000,function(){
-       setTimeOut(timeOut,3000);
-    });
-
-   
+    $('#container').html(el.html());
 });
