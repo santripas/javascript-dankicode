101bc536d8b539cf2386647d59ae052b27741a40
Sandro Silva <81268287+santripas@users.noreply.github.com>
2023-05-17 20:42:37 -0300

    estudando tipos de validação

--- JQuery/js/functions.js
+++ JQuery/js/functions.js
@@ -1,5 +1,11 @@
 $(function(){
     
+    var frase = 'Ola mundo';
+    var verifica = frase.match(/Ola/);
+
+    console.log(verifica);
+
+
     abrirJanela();
     verificarCliqueFechar();
 
@@ -23,6 +29,37 @@ $(function(){
             e.stopPropagation();
         })
 
+    }
+
+    $('form#form1').submit(function(e){
+        e.preventDefault();
+
+        var nome = $('input[name=nome]').val();
+        var telefone = $('input[name=telefone]').val();
+        var email = $('input[name=email]').val();
+
+        var amount = nome.split(' ').length;
+        var splitStr = nome.split(' ');
+        if(amount >= 2){
+            for(var i = 0; i < amount; i++ ){
+                if(splitStr[i].match(/^[A-Z]{1}[a-z]{1,}$/)){
+                    console.log("Nossa condição bateu, podemos dar continuidade");
+                }else{
+                    aplicarCampoInvalido($('input[name=nome'));
+                    return false;
+                }
+            }
+        }else{
+            aplicarCampoInvalido($('input[name=nome'));
+            return false;
+        }        
+
+    })
+
+    function aplicarCampoInvalido(el){
+        el.css('border','2px solid red');
+        el.data('invalido','true');
+        el.val('Campo inválido!');
     }
  
 });
