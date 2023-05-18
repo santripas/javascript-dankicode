65d602aee76a7ab74a37743946555d3db361e584
Sandro Silva <81268287+santripas@users.noreply.github.com>
2023-05-18 19:51:50 -0300

    animando elementos com scrolltop

--- JQuery/js/functions.js
+++ JQuery/js/functions.js
@@ -1,65 +1,18 @@
 $(function(){
     
-    var frase = 'Ola mundo';
-    var verifica = frase.match(/Ola/);
-
-    console.log(verifica);
-
-
-    abrirJanela();
-    verificarCliqueFechar();
-
-    function abrirJanela(){
-        $('.btn').click(function(e){
-            e.stopPropagation();
-            $('.bg').fadeIn();
-        });
-
-    }
-
-    function verificarCliqueFechar(){
-
-        var el = $('body,.closeBtn');
-
-        el.click(function(){
-            $('.bg').fadeOut();
-        })
-
-        $('.form').click(function(e){
-            e.stopPropagation();
-        })
-
-    }
-
-    $('form#form1').submit(function(e){
-        e.preventDefault();
-
-        var nome = $('input[name=nome]').val();
-        var telefone = $('input[name=telefone]').val();
-        var email = $('input[name=email]').val();
-
-        var amount = nome.split(' ').length;
-        var splitStr = nome.split(' ');
-        if(amount >= 2){
-            for(var i = 0; i < amount; i++ ){
-                if(splitStr[i].match(/^[A-Z]{1}[a-z]{1,}$/)){
-                    console.log("Nossa condição bateu, podemos dar continuidade");
-                }else{
-                    aplicarCampoInvalido($('input[name=nome'));
-                    return false;
-                }
+    $(window).scroll(function(){
+        $('.sessao').each(function(){
+            var windowOffY = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            var elOffY = $(this).offset().top;
+            if(elOffY+30 < (windowOffY + windowHeight) && elOffY+30+$(this).height() > windowOffY){
+                $('a').css('border-bottom','0');
+                var target = $(this).attr('target');
+                $('.'+target).css('border-bottom','2px solid #333');
+                return;
             }
-        }else{
-            aplicarCampoInvalido($('input[name=nome'));
-            return false;
-        }        
 
+        });
     })
-
-    function aplicarCampoInvalido(el){
-        el.css('border','2px solid red');
-        el.data('invalido','true');
-        el.val('Campo inválido!');
-    }
  
 });
