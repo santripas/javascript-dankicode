3c05216261f0ecae923f1533463f9e61e28c6a25
Sandro Silva <81268287+santripas@users.noreply.github.com>
2023-05-21 21:36:12 -0300

    testando uma forma de fazer slider responsivo

--- JQuery/js/functions.js
+++ JQuery/js/functions.js
@@ -1,18 +1,4 @@
 $(function(){
     
-    $(window).scroll(function(){
-        $('.sessao').each(function(){
-            var windowOffY = $(window).scrollTop();
-            var windowHeight = $(window).height();
-            var elOffY = $(this).offset().top;
-            if(elOffY+30 < (windowOffY + windowHeight) && elOffY+30+$(this).height() > windowOffY){
-                $('a').css('border-bottom','0');
-                var target = $(this).attr('target');
-                $('.'+target).css('border-bottom','2px solid #333');
-                return;
-            }
-
-        });
-    })
  
 });
