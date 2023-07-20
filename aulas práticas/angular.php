cdc77f5d1714755fd623c02b9864c38a18549186
Santripas <81268287+santripas@users.noreply.github.com>
2023-07-19 21:08:23 -0300

    praticando angular

---
+++ aulas práticas/angular.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Angular JS</title>
+</head>
+<body>
+
+
+
+<p>A soma de 5+5 é: {{ 5 + 5 }}</p>
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+</body>
+</html>
